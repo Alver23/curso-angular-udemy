a3d1ad941ebc08dbfc6d6506917e38594dac17e3
Alexander Grisales <alexander.grisales@almundo.com>
2018-04-30 09:31:57 -0500

    Templates string

--- Clase1/app.js
+++ Clase1/app.js
@@ -1,27 +1,6 @@
 "use strict";
-function saludar(nombre) {
-    alert("Hola " + nombre.toUpperCase());
-}
-var wolverine = {
-    nombre: "Logan"
-};
-saludar(wolverine.nombre);
-var nombre = "Peter";
-var numero = 123;
-var bool = true;
-var hoy = new Date();
-hoy = new Date('2020-10-21');
-var cualquiera;
-cualquiera = nombre;
-cualquiera = numero;
-cualquiera = bool;
-cualquiera = hoy;
-var spiderman = {
-    nombre: "Peter",
-    edad: 20,
-};
-spiderman = {
-    nombre: "Juan",
-    edad: 30,
-    apellido: "perez",
-};
+var nombre = "Juan";
+var apellido = "Perez";
+var edad = 32;
+var texto = "Hola, \n    " + nombre + " " + apellido + "\n    (" + edad + ")";
+alert(texto);
--- Clase1/app.ts
+++ Clase1/app.ts
@@ -1,34 +1,9 @@
-function saludar(nombre: string){
-    alert("Hola " + nombre.toUpperCase());
-}
+let nombre:string = "Juan";
+let apellido:string = "Perez";
+let edad:number = 32
 
-var wolverine = {
-    nombre: "Logan"
-};
+let texto = `Hola, 
+    ${nombre} ${apellido}
+    (${edad})`;
 
-saludar(wolverine.nombre);
-
-let nombre:string = "Peter";
-let numero:number = 123;
-let bool:boolean = true;
-let hoy:Date = new Date();
-
-hoy = new Date('2020-10-21');
-
-let cualquiera:any;
-cualquiera = nombre;
-cualquiera = numero;
-cualquiera = bool;
-cualquiera = hoy;
-
-let spiderman = {
-    nombre: "Peter",
-    edad: 20,
-};
-
-spiderman = {
-    nombre: "Juan",
-    edad: 30,
-    apellido: "perez",
-}
-
+alert(texto);
