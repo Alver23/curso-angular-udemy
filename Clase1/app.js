0ecfd189cd1cb7173aee45a3cb6ff6764cb72a1d
Alexander Grisales <alexander.grisales@almundo.com>
2018-04-30 11:17:53 -0500

    Remove folder clase 1

--- Clase1/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var nombre = "Juan";
-var apellido = "Perez";
-var edad = 32;
-var texto = "Hola, \n    " + nombre + " " + apellido + "\n    (" + edad + ")";
-alert(texto);
--- Clase1/app.ts
+++
@@ -1,9 +0,0 @@
-let nombre:string = "Juan";
-let apellido:string = "Perez";
-let edad:number = 32
-
-let texto = `Hola, 
-    ${nombre} ${apellido}
-    (${edad})`;
-
-alert(texto);
